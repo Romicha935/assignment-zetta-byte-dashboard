f991b02d3cbacad0f1360f43cae2f529b9736688
romicha-parvin <romichaparvin35@gmail.com>
2025-09-18 00:21:12 +0600

    footer added

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,16 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer className='w-ull bg-green-200  px-6 py-4 mt-8 text-center justify-center'>
+        <div className='flex gap-4 mt-2 md:mt-0'>
+         <p>© {new Date().getFullYear()} MyDashboard. All rights reserved.</p>
+         <div>
+            <a href="#" className='hover:underline' >Privecy Policy</a>
+            <a href="#" className='hover:underline' > Terms of Service</a>
+         </div>
+
+        </div>
+    </footer>
   )
 }
 
