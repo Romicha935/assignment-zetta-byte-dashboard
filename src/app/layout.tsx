56ad0162e6672244ec7f8f1550f464f0b8ffb8ef
romicha-parvin <romichaparvin35@gmail.com>
2025-09-18 01:28:01 +0600

    sidebar added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,25 +1,31 @@
-
+"use client"
 import "./globals.css";
 import Footer from "@/components/Footer";
 import Navbar from "@/components/Navbar";
+import Sidebar from "@/components/sidebar";
 
 
-export const metadata = {
-  title: "Zettabyte Dashboard",
-};
+
+// export const metadata = {
+//   title: "Zettabyte Dashboard",
+// };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body className="bg-slate-100">
-        <div className=" min-h-screen flex flex-col">
+        <div className=" min-h-screen flex">
+         {/* Sidebar */}
+          <Sidebar/>
+        <div className="  flex-1 flex flex-col">
           <Navbar/>
-          {/* Sidebar */}
-          {/* <Sidebar /> */}
+         
           {/* Main content */}
           <main className="flex-1 p-6">{children}</main>
           <Footer/>
         </div>
+        </div>
+        
       </body>
     </html>
   );
--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -27,3 +27,34 @@
 //     </motion.aside>
 //   );
 // }
+
+"use client "
+import { spring } from 'framer-motion'
+import React, { useState } from 'react'
+import { motion } from 'framer-motion';
+import Link from 'next/link';
+
+const Sidebar = () => {
+    const [open ,setOpen] = useState(true)
+  return (
+    <motion.aside 
+    animate={{width: open ? 220 : 60 }} 
+    transition={{type:spring, stiffness: 220 , damping: 20}}
+    className="h-screen  bg-gradient-to-b from-green-500 to-purple-500"
+    >
+        <div className='flex items-center justify-between p-3'>
+             <span className="font-bold">{open ? "Zettabyte" : "ZB"}</span>
+            <button onClick={()=> setOpen(!open)} className='text-sm'>{open ? "◀" : "▶"  }</button>
+        </div>
+
+        <nav className='flex flex-col gap-2 p-2'>
+            <Link href="/" className='hover:bg-white text-center px-3 py-3 rounded' >Dashboard</Link>
+            <Link href="/posts" className='hover:bg-white text-center px-3 py-3 rounded' >Posts</Link>
+            <Link href="/users" className='hover:bg-white text-center px-3 py-3 rounded' >Users</Link>
+        </nav>
+
+    </motion.aside>
+  )
+}
+
+export default Sidebar
